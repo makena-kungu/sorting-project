252de220b52cf64cd98f7a4938759b9d8384ec28
kinuthiake <kinuthiakeys@gmail.com>
2022-07-04 14:23:38 +0300

    the test class and Bursary class

--- src/main/java/com/erickandedwin/usecase/Bursary.java
+++ src/main/java/com/erickandedwin/usecase/Bursary.java
@@ -2,73 +2,60 @@ package com.erickandedwin.usecase;
 
 import org.jetbrains.annotations.NotNull;
 
-public record Bursary(String name, String school,
+public record Bursary(String name,
+                      String school,
                       String grade,
                       double income,
-                      int parentStatus, double weightedMean) implements Comparable<Bursary>{
+                      String parentStatus) implements Comparable<Bursary>{
     public double weightedMean() {
-        double weightedMean = (grade+ income+parentStatus )/3;
-        return 0;
+        return (getGrade() + getIncome() + getParentStatus() )/3;
     }
         private int getGrade () {
-            int g = 0;
-            switch (grade) {
-                case "A":
-                    g = 12;
-                case "A-":
-                    g = 11;
-                case "B+":
-                    g = 10;
-                case "B":
-                    g = 9;
-                case "B-":
-                    g = 8;
-                case "C+":
-                    g = 7;
-                case "C":
-                    g = 6;
-                case "C-":
-                    g = 5;
-                case "D+":
-                    g = 4;
-                case "D":
-                    g = 3;
-                case "D-":
-                    g = 2;
-                case "E":
-                    g = 1;
-
-            }
-            return g;
+            return switch (grade) {
+                case "A" -> 12;
+                case "A-" -> 11;
+                case "B+" -> 10;
+                case "B" -> 9;
+                case "B-" -> 8;
+                case "C+" -> 7;
+                case "C" -> 6;
+                case "C-" -> 5;
+                case "D+" -> 4;
+                case "D" -> 3;
+                case "D-" -> 2;
+                case "E" -> 1;
+                default -> throw new IllegalArgumentException();
+            };
         }
         private double getIncome () {
             int i = 0;
-            if (income < 5000) {
-                i = 12;
+            if (income <= 5000) {
+                i = 20;
             }
-            if ((5001 < income) < 10000) {
+            else if ( income <= 10000) {
                 i = 10;
             }
-            if (income > 10000) {
+            else if(income > 10000) {
                 i = 8;
             }
             return i;
 
         }
         private int getParentStatus () {
-            int p = 0;
-            switch (parentStatus) {
-                case "Orphan":
-                    p = 12;
-                case "not Orphan":
-                    p = 6;
-            }
-            return p;
+            return switch (parentStatus) {
+                case "Orphan" -> 12;
+                case "Not Orphan" -> 6;
+                default -> throw new IllegalArgumentException("Unexpected value "+ parentStatus);
+            };
         }
 
 
         @Override
-        public int compareTo (@NotNull Bursary o){
+        public int compareTo (@NotNull Bursary o) {
+            int m = Double.compare(weightedMean(), o.weightedMean());
+            if (m != 0) {
+                return -m;
+            }
             int n = name.compareTo(o.name);
             if (n != 0) {
                 return n;
@@ -77,21 +64,7 @@ public record Bursary(String name, String school,
             if (s != 0) {
                 return s;
             }
-            int g = grade.compareTo(o.grade);
-            if (g != 0) {
-                return g;
-            }
-            double i = double.compare(income, o.income);
-
-            if (i != 0) {
-                return (int) i;
-            }
-            int p = Integer.compare(parentStatus, o.parentStatus);
-
-            return p;
+            return grade.compareTo(o.grade);
         }
-        () {
-            //weightedMean =  sum/a
-            return 0;
-        }
+
     }
--- src/main/java/com/erickandedwin/usecase/TestKit.java
+++ src/main/java/com/erickandedwin/usecase/TestKit.java
@@ -1,27 +1,34 @@
 package com.erickandedwin.usecase;
 
+import com.erickandedwin.HybridMergeSort;
 import com.erickandedwin.PairSort;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class TestKit {
-    public static Bursary[] generateList(int size){
-        var bursary= new Bursary[size];
-        var rnd= new Random();
-        for (int i =0; i<size; i++ ){
-            bursary[i]= new Bursary("Name" + i%3,"School"+ i%3,"Grade", rnd.nextDouble(0, 40000),"ParentStatus", );
+    public static Bursary[] generateList(int size) {
+        var grades = new String[]{"A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "E"};
+        var bursary = new Bursary[size];
+        var rnd = new Random();
+        for (int i = 0; i < size; i++) {
+            bursary[i] = new Bursary("Name" + rnd.nextInt((2*size)/3), "School" + rnd.nextInt((2*size)/3), grades[rnd.nextInt(0, 11)], rnd.nextDouble(0, 40000), rnd.nextBoolean()?"Orphan":"Not Orphan");
 
         }
         return bursary;
 
     }
-    public static void main(String[] args) {
-        var Bursary = generateList(10);
-        PairSort.pairSort(bursary);
 
-        for (Bursary person:bursary){
-            System.out.println(bursary);
+    public static void main(String[] args) {
+        var bursaries = generateList(50);
+        System.out.println(Arrays.toString(bursaries));
+        HybridMergeSort.hybridSort(bursaries);
+        for (Bursary bursary : bursaries) {
+            System.out.println(bursary+ " " + bursary.weightedMean());
+
         }
+
+
 
     }
 }
